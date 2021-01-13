5bb306e3ab14eda72beb7fbea7c40af9eb76ae83
Rich Alossi <alossix@gmail.com>
2021-01-13 20:35:47 +0100

    Highlight card changes

--- components/Main/Highlight/Regions/Piedmont.js
+++ components/Main/Highlight/Regions/Piedmont.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 
 const Piedmont = () => {
   return (
-<<<<<<< HEAD
     <Link href="/regions#piedmont">
       <div className="highlight-card">
         <h4 className="highlight-card-title">
@@ -32,36 +31,6 @@ const Piedmont = () => {
         </div>
       </div>
     </Link>
-=======
-    <div className="highlight-card">
-      <h4 className="highlight-title">February: Piedmont</h4>
-      <div className="highlight-image">
-        <Image
-          src="/highlights/piedmont.jpg"
-          alt="Piedmont Wine Region of Italy"
-          width={250}
-          height={250}
-          objectFit={'cover'}
-        />
-        <div className="highlight-card-container">
-          <p>
-            February highlights three new wines from the Piedmont
-            region:
-          </p>
-          <ul>
-            <li>Barolo</li>
-            <li>Barbera</li>
-            <li>Moscato d'Asti</li>
-          </ul>
-        </div>
-        <Link href="/regions/piedmont">
-          <a className="highlight-card-link">
-            Read more about Piedmont
-          </a>
-        </Link>
-      </div>
-    </div>
->>>>>>> 85fb0eaceb1dd13ffb49fdee0bd56b93905c72d3
   );
 };
 
