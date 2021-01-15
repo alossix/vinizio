1922c1dd97e49e0b4a225b38d903381ae43faa38
Rich Alossi <alossix@gmail.com>
2021-01-15 16:50:09 +0100

    Style changes on main page component

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,55 +1,18 @@
 import React from 'react';
-import Image from 'next/image';
+import Hero from './Hero/Hero';
+import Description from './Description/Description';
 import Highlight from './Highlight/Highlight';
 
 const Main = () => {
   return (
     <main>
-      <section className="main-hero">
-        <h1>
-          Hand-selected small-batch Italian wines delivered to your
-          doorstep each month.
-        </h1>
-        <div className="main-hero-text">
-          <h2>
-            Start your Italian wine journey with Vinizio! <br />
-            Three new wines each month, every delivery highlighting a
-            unique wine region of Italy.
-          </h2>
-        </div>
-      </section>
-      <section className="main-description">
-        <h3>With your monthly subscription:</h3>
-        <ul>
-          <li>
-            Emphasis on small-batch vintners and traditional methods,
-            fantastic way to discover new varieties not commonly
-            available at your local supermarket.
-          </li>
-          <li>
-            Discover the best hand-selected wines from independent
-            producers
-          </li>
-          <li>
-            Support traditional wine production throughout the Italian
-            peninsula
-          </li>
-          <li>
-            Learn about where your wine originates, the story of the
-            producer, and what makes your bottle so special
-          </li>
-          <li>
-            Pair your discoveries with traditional regional recipes,
-            included with your order
-          </li>
-          <li>No-hassle cancellation at any time</li>
-        </ul>
-        <h2>
-          Easily set your choice between reds, whites, or a mix of
-          both. Unique gift subscription for the wine lover. Shipping
-          throughout the E.U.
-        </h2>
-      </section>
+      <Hero />
+      <Description />
+      <h2>
+        Easily set your choice between reds, whites, or a mix of both.
+        Unique gift subscription for the wine lover. Shipping
+        throughout the E.U.
+      </h2>
       <section className="signup-area">
         <button>Sign Up Now!</button>
       </section>
