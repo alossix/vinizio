fe5cc46f50ef279934d375a9a861fb0adf4bfc69
Rich Alossi <alossix@gmail.com>
2021-01-22 18:39:54 +0100

    Add user database query/API call

--- models/User.model.js
+++ models/User.model.js
@@ -10,31 +10,42 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
+    default: '',
   },
   lastName: {
     type: String,
     required: true,
+    default: '',
   },
   telephone: {
     type: String,
+    default: '',
   },
   country: {
     type: String,
     required: true,
+    default: '',
   },
   address: {
     city: {
       type: String,
       required: true,
+      default: '',
     },
     streetAndNumber: {
       type: String,
       required: true,
+      default: '',
     },
     postcode: {
       type: String,
       required: true,
+      default: '',
     },
+  },
+  updated: {
+    type: Boolean,
+    default: false,
   },
 });
 
